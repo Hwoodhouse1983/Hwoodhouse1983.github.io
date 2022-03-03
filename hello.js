c1f0c8942c84e1d43eda2c3d3509425332db79f7
Hayley Woodhouse <79152442+Hwoodhouse1983@users.noreply.github.com>
2022-03-03 12:11:10 +0000

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+function display() {
+   alert("Hello there and thanks for visiting Cat Of Nine Tails!");
+}
+
+function display() {
+   alert("Contact us to enquire about custom designs in your size / to your specifications");
+}
+
